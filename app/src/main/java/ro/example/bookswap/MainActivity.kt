c0c86ea2ab5f89d632b217f5341f0bb0bd635bb0
Mihai Mihaila <mihai-gabriel.mihaila@my.fmi.unibuc.ro>
2021-06-18 15:25:14 +0300

    Bottom navigation added

--- app/src/main/java/ro/example/bookswap/MainActivity.kt
+++ app/src/main/java/ro/example/bookswap/MainActivity.kt
@@ -1,11 +1,55 @@
 package ro.example.bookswap
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.navigation.NavigationBarView
+
+const val EXTRA_MESSAGE = "ro.example.bookswap.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toast: Toast = Toast.makeText(applicationContext, "text", Toast.LENGTH_SHORT)
+
+        val navigationBarView: NavigationBarView = findViewById(R.id.bottom_nav)
+
+
+
+        navigationBarView.setOnItemSelectedListener { item ->
+
+            when (item.itemId) {
+                R.id.page_1 -> {
+                    toast.setText("Page 1")
+                    toast.show()
+                    true
+                }
+                R.id.page_2 -> {
+                    toast.setText("Page 2")
+                    toast.show()
+                    true
+                }
+                R.id.page_3 -> {
+                    toast.setText("Page 3")
+                    toast.show()
+                    true
+                }
+                R.id.page_4 -> {
+                    toast.setText("Page 4")
+                    toast.show()
+                    true
+                }
+                else -> false
+            }
+        }
+
     }
+
+
+
 }
